2b5d347513cfc00eb101c84ef158d61920f86601
Midhunpm6 <midhunpm6060@gmail.com>
2025-09-06 14:30:08 +0530

    Create order service flow

---
+++ src/order-service/index.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import cors from 'cors'
+import connectDB from './DB/config.js'
+import { connectProducer } from './utils/kafka.js'
+
+const app = express()
+const PORT = process.env.PORT || 6000
+
+const startServer = async () => {
+  try {
+    await connectProducer()
+    console.log('Kafka producer connected ')
+
+    await connectDB()
+    console.log('MongoDB connected ')
+
+    app.use(cors())
+    app.use(express.json())
+
+    app.get('/', (req, res) => {
+      res.send('Catalog and Inventory Service is running')
+    })
+   
+
+    app.listen(PORT, () => {
+      console.log(`Catalog and Inventory Service is running on port ${PORT}`)
+    })
+  } catch (err) {
+    console.error('Failed to start server:', err)
+    process.exit(1)
+  }
+}
+
+startServer()
